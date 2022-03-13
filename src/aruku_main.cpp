fc6754111d695d5f6c55cfd2eb28ae3c153745e0
maroqijalil <jalilpro31@gmail.com>
2022-03-13 13:40:40 +0700

    feat: add aruku_main

---
+++ src/aruku_main.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2021 Ichiro ITS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <memory>
+
+#include "aruku/walking/node/walking_manager.hpp"
+#include "aruku/node/aruku_node.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<rclcpp::Node>("aruku_node");
+  auto aruku_node = std::make_shared<aruku::ArukuNode>(node);
+
+  auto walking_manager = std::make_shared<aruku::WalkingManager>();
+  walking_manager->load_data("");
+
+  aruku_node->set_walking_manager(walking_manager);
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}

6f69caa406b91364ecfcee43a74eb23ac2e0f8c7
hanunshaka <hanunshaka02@gmail.com>
2022-06-25 18:48:37 +0700

    feat: use argument to specify tachimawari or robocup_client

--- launch/aruku_launch.py
+++ launch/aruku_launch.py
@@ -1,19 +1,76 @@
+from launch_ros.actions import Node
+
 from launch import LaunchDescription
-from launch_ros.actions import Node;
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+    firstPackage = LaunchConfiguration('firstPackage')
+    secondPackage = LaunchConfiguration('secondPackage')
+    host = LaunchConfiguration('host')
+    port = LaunchConfiguration('port')
+
+    firstPackage_launch_arg = DeclareLaunchArgument(
+        'firstPackage',
+        default_value='aruku'
+    )
+
+    firstNode = Node(
+        package=firstPackage,
+        executable='main',
+        name='aruku',
+        arguments=['src/aruku/data/']
+    )
+
+    run_first_package = ExecuteProcess(
+        cmd=[[
+            'ros2 run ',
+            'aruku ',
+            'main ',
+            'src/aruku/data/'
+        ]],
+        shell=True
+    )
+
+    secondPackage_launch_arg = DeclareLaunchArgument(
+        'secondPackage',
+        default_value='tachimawari'
+    )
+
+    host_launch_arg = DeclareLaunchArgument(
+        'host',
+        default_value=''
+    )
+
+    port_launch_arg = DeclareLaunchArgument(
+        'port',
+        default_value=''
+    )
+
+    secondNode = Node(
+        package=secondPackage,
+        executable='main',
+        arguments=[],
+    )
+
+    run_second_package = ExecuteProcess(
+        cmd=[[
+            'ros2 run ',
+            secondPackage,
+            ' main ',
+            host,
+            port
+        ]],
+        shell=True
+    )
+
     return LaunchDescription([
-        Node(
-            package='aruku',
-            namespace='aruku',
-            executable='main',
-            name='aruku',
-            arguments=['src/aruku/data/'],
-        ),
-        Node(
-            package='tachimawari',
-            namespace='tachimawari',
-            executable='main',
-            name='tachimawari',
-        )
+        firstPackage_launch_arg,
+        secondPackage_launch_arg,
+        host_launch_arg,
+        port_launch_arg,
+        firstNode,
+        secondNode,
+        run_first_package,
+        run_second_package
     ])

ba1c0e9019dc08bc76b65c51b9ebd7abbe58dfa3
Azielh <azielh473@gmail.com>
2022-11-16 23:03:26 +0700

    feat: add get_angle test

---
+++ test/kinematic_test.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 ICHIRO ITS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "gtest/gtest.h"
+
+#include "kinematic_test.hpp"
+
+namespace ar = aruku;
+
+TEST(InitPositionTest, GetAngles)
+{
+  float max_error = 0.5;
+
+  float current_angles[20] = ar::Kinematic::get_angles();
+
+  for (int i = 0; i < 20; i++) {
+    EXPECT_NEAR(0.0, current_angles[i], max_error);
+  }
+}

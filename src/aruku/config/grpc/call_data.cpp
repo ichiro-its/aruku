4b3bf97698c9f07446dfd8c2ae6cf49dfa135ad3
marfanr <mohammadarfan45@gmail.com>
2023-11-06 10:14:11 +0700

    feat: move proto compile part to aruku interfaces

--- src/aruku/config/grpc/call_data.cpp
+++ src/aruku/config/grpc/call_data.cpp
@@ -1,6 +1,27 @@
+// Copyright (c) 2023 Ichiro ITS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #include "aruku/config/grpc/call_data.hpp"
 
-namespace aruku {
+namespace aruku
+{
 template <class ConfigRequest, class ConfigReply>
 CallData<ConfigRequest, ConfigReply>::CallData(
   aruku_interfaces::proto::Config::AsyncService * service, grpc::ServerCompletionQueue * cq,
@@ -30,4 +51,4 @@ template class CallData<aruku_interfaces::proto::SetWalking, aruku_interfaces::p
 template class CallData<aruku_interfaces::proto::ConfigWalking, aruku_interfaces::proto::Empty>;
 template class CallData<aruku_interfaces::proto::Empty, aruku_interfaces::proto::ConfigWalking>;
 
-} // namespace aruku
+}  // namespace aruku

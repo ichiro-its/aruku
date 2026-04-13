ebb38366c82ab10a6c29cc0799ee1221c68a694c
shikajiko <shikajiko@github.com>
2026-04-13 21:04:54 +0700

    fix: reduce clamping values

--- src/aruku/walking/node/walking_manager.cpp
+++ src/aruku/walking/node/walking_manager.cpp
@@ -376,7 +376,7 @@ bool WalkingManager::process()
       pid_offset_pitch = keisan::clamp(pid_offset_pitch, -60.0, 60.0);
 
       pid_offset_roll = p_roll_gain * roll_error + i_roll_gain * roll_integral + d_roll_gain * roll_derivative;
-      pid_offset_roll = keisan::clamp(pid_offset_roll, -180.0, 180.0);
+      pid_offset_roll = keisan::clamp(pid_offset_roll, -120.0, 120.0);
 
       prev_pitch_error = pitch_error;
       prev_roll_error = roll_error;
